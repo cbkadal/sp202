813f3f0001f83aeb485e76448d0bdcd35192c53b
Cicak Bin Kadal <cbkadal@DELETE.vlsm.org>
2020-10-07 12:27:43 +0700

    SP202 cbkadal

---
+++ SandBox/misc/proc01.c
@@ -0,0 +1,21 @@
+/*
+ * Wed 07 Oct 2020 05:18:34 AM UTC
+ * Wed 30 Sep 2020 01:00:25 PM WIB
+ */
+
+#include <stdio.h>
+#include <gnu/libc-version.h>
+
+int main(void) {
+    printf("Hello, the libc version is: %s\n", gnu_get_libc_version());
+    long ii=672274793;
+    printf("%9.9ld -- %#8.8lX (Linus Torvalds)\n", ii, ii);
+    ii=85072278;
+    printf("%9.9ld -- %#8.8lX (PM Torvalds) \n", ii, ii);
+    ii=369367448;
+    printf("%9.9ld -- %#8.8lX (DY Torvalds)\n", ii, ii);
+    ii=537993216;
+    printf("%9.9ld -- %#8.8lX (CA Torvalds)\n", ii, ii);
+}
+
+
