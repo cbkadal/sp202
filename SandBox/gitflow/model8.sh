42a372d150abc251a1951ee7cb2784f2a7ea6572
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@deleteThis.vlsm.org>
2020-10-28 11:13:47 +0700

    modified:   model8.sh

--- SandBox/gitflow/model8.sh
+++ SandBox/gitflow/model8.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# REV03 Wed Oct 28 11:13:07 WIB 2020
 # REV02 Sun 25 Oct 2020 11:36:35 AM WIB
 # REV01 Sat 24 Oct 2020 07:34:58 PM WIB
 # START Sun 18 Oct 2020 06:19:53 AM WIB (rms)
@@ -10,11 +11,11 @@
 # user2 will work in branch user2
 # user3 will work in branch user3
 
-git config gitflow.prefix.hotfix "hotfix/"
-git config gitflow.prefix.feature "feature/"
-git config gitflow.prefix.bugfix "bugfix/"
-git config gitflow.prefix.release "release/"
-git config gitflow.prefix.support "support/"
+git config --global gitflow.prefix.hotfix "hotfix/"
+git config --global gitflow.prefix.feature "feature/"
+git config --global gitflow.prefix.bugfix "bugfix/"
+git config --global gitflow.prefix.release "release/"
+git config --global gitflow.prefix.support "support/"
 
 USERS="user1 user2 user3"
 # This will NUKE ALL user1, user2, user2, remote, master, develop
